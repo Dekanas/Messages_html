61b662e00639ea6002ccc8871c9a9487e6bad322
Dekanas <44368441+Dekanas@users.noreply.github.com>
2023-03-30 16:19:55 +0300

    Update messages_html.py

--- messages_html.py
+++ messages_html.py
@@ -10,9 +10,9 @@ html_code = st.text_area("Paste HTML code here", height=300)
 # Add a button to render the HTML code
 if st.button("Render Preview"):
     # Write the HTML code to a temporary file
-    with open("temp.html", "w") as f:
-        f.write(html_code)
+    with open("temp.html", "w", encoding='utf-8') as f:
+        f.write(f"<!DOCTYPE html><html><head><meta charset='utf-8'><title>Preview</title></head><body>{html_code}</body></html>")
     
     # Use an IFrame to display the HTML preview
-    preview_url = f"data:text/html;base64,{base64.b64encode(open('temp.html', 'r').read().encode()).decode()}"
+    preview_url = f"data:text/html;base64,{base64.b64encode(open('temp.html', 'r', encoding='utf-8').read().encode()).decode()}"
     st.components.v1.iframe(src=preview_url, height=500, scrolling=True)
